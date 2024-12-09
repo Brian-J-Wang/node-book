d238db61022ba3269c05fe42ee0609b609a87273
Brian Wang <brianwang7102@gmail.com>
2024-12-09 11:11:11 -0500

    feat: check-list shows number

--- src/components/form/form-components/check-list/check-list.tsx
+++ src/components/form/form-components/check-list/check-list.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { generateObjectId } from "../../../../utils/uuidGen"
 
 import trashIcon from "../../../../assets/trash-icon.svg"
@@ -21,6 +21,7 @@ type checkListProps = {
 
 const CheckList = (props: checkListProps) => {
     const [ internalId ] = useState(generateObjectId());
+    const [ numComplete, setNumComplete] = useState<number>(0);
     const [ items, setItems ] = useState<checkListItem[]>([
         {
             id: generateObjectId(),
@@ -33,6 +34,18 @@ const CheckList = (props: checkListProps) => {
             blurb: 'Finish that'
         },
     ]);
+
+    useEffect(() => {
+        //set the number complete;
+        let count = 0;
+        props.content.forEach((item) => {
+            if (item.checked) {
+                count++;
+            }
+        });
+
+        setNumComplete(count);
+    }, [])
 
     const handleKeyboardInput = (evt: React.KeyboardEvent) => {
         if (evt.key == "Enter") {
@@ -52,15 +65,32 @@ const CheckList = (props: checkListProps) => {
         setItems(items.filter(item => item.id != id));
     } 
 
+    const handleCheck = (evt: React.ChangeEvent) => {
+        setItems(items.map((item) => {
+            if (item.id == evt.target.id) {
+                item.checked = (evt.target as HTMLInputElement).checked;
+            }
+
+            return item;
+        }));
+
+        if ((evt.target as HTMLInputElement).checked) {
+            setNumComplete((value) => value + 1);
+        } else {
+            setNumComplete((value) => value - 1);
+        }
+    }
+
     return (
         <div>
             <div className="check-list__header" hidden={props.showHeader ?? false}>
                 <h4 className="check-list__title">SubTasks</h4>
-                <small>0/7</small>
+                <small>{`${numComplete}/${items.length}`}</small>
             </div>
             <div className="check-list__body">
                 {
-                    items.map((item) => <CheckListItem key={item.id} content={item} name={internalId} onDelete={handleRemoveItem}/>)
+                    items.map((item) => <CheckListItem key={item.id} id={item.id} content={item} 
+                    name={internalId} onDelete={handleRemoveItem} onChange={handleCheck}/>)
                 }
                 <div>
                     <input type="text" placeholder="Click here to add more" className="check-list__input" onKeyDown={handleKeyboardInput}/>
@@ -73,13 +103,15 @@ const CheckList = (props: checkListProps) => {
 type checkListItemProps = {
     content: checkListItem
     name: string;
+    id: string;
     onDelete: (id: string) => void;
+    onChange: (evt: React.ChangeEvent) => void;
 }
-const CheckListItem = ({content, onDelete}: checkListItemProps) => {
+const CheckListItem = ({content, id, onDelete, onChange}: checkListItemProps) => {
     return (
         <div className="check-list-item">
             <label htmlFor={content.id} className="check-list-item__label">
-                <input type="checkbox"/>
+                <input type="checkbox" id={id} onChange={onChange} checked={content.checked}/>
                 <p className="check-list-item__blurb">{content.blurb}</p>
             </label>
             <img src={trashIcon} alt="delete" className="check-list-item__delete" onClick={() => {
