0ef50190335c18040f8344d5894e785c0b4a8447
Brian Wang <brianwang7102@gmail.com>
2025-01-05 11:42:01 -0500

    fix: incomplete code in item-node-validator

--- src/components/node/item-node/item-node-validator.ts
+++ src/components/node/item-node/item-node-validator.ts
@@ -2,8 +2,7 @@ import { Node, Graph } from "../../../utils/graph";
 import NodeObject from "../node-object";
 
 function validateItemNode(node: Node<NodeObject>, graph: Graph<NodeObject>): boolean {
-    //item nodes cannot connect to upstream nodes
-    graph.
+
 
     node.content.builder().validationObject({
         isValid: true,
